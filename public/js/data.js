d65610c2ca33688df915eef50ba9fa0cb6581b2b
Rassal20 <57297787+Rassal20@users.noreply.github.com>
2025-07-04 21:49:17 +0400

    Add files via upload

--- public/js/data.js
+++ public/js/data.js
@@ -118,7 +118,7 @@ const countryData = [
   { name: 'Latvia', code: 'LV', phone: '371' },
   { name: 'Lebanon', code: 'LB', phone: '961' },
   { name: 'Lesotho', code: 'LS', phone: '266' },
-  { name: 'Liberia', code: 'LR', phone: '231' },
+  { name:: 'Liberia', code: 'LR', phone: '231' },
   { name: 'Libya', code: 'LY', phone: '218' },
   { name: 'Liechtenstein', code: 'LI', phone: '423' },
   { name: 'Lithuania', code: 'LT', phone: '370' },
@@ -240,9 +240,48 @@ const countryData = [
   { name: 'Western Sahara', code: 'EH', phone: '212' },
   { name: 'Yemen', code: 'YE', phone: '967' },
   { name: 'Zambia', code: 'ZM', phone: '260' },
-  { name: 'Zimbabwe', code: 'ZW', phone: '263' },
-];
+  { name: 'Zimbabwe', code: 'ZW', phone: '263' }
+].sort((a, b) => a.name.localeCompare(b.name));
 
 const vehicleData = {
     "Acura": ["ILX", "MDX", "NSX", "RDX", "RLX", "TLX"], "Alfa Romeo": ["4C", "Giulia", "Stelvio"], "Audi": ["A3", "A4", "A5", "A6", "A7", "A8", "Q3", "Q5", "Q7", "Q8", "R8", "TT", "e-tron"], "Bentley": ["Bentayga", "Continental GT", "Flying Spur"], "BMW": ["2 Series", "3 Series", "4 Series", "5 Series", "7 Series", "8 Series", "i3", "i4", "i8", "iX", "X1", "X2", "X3", "X4", "X5", "X6", "X7", "Z4"], "Buick": ["Enclave", "Encore", "Envision"], "Cadillac": ["CT4", "CT5", "Escalade", "XT4", "XT5", "XT6"], "Chevrolet": ["Blazer", "Bolt EV", "Camaro", "Corvette", "Equinox", "Malibu", "Silverado", "Suburban", "Tahoe", "Traverse", "Trax"], "Chrysler": ["300", "Pacifica", "Voyager"], "Dodge": ["Challenger", "Charger", "Durango", "Hornet"], "Ferrari": ["296 GTB", "812 Superfast", "F8", "Portofino M", "Roma", "SF90 Stradale"], "Fiat": ["500X"], "Ford": ["Bronco", "Edge", "Escape", "Expedition", "Explorer", "F-150", "Mustang", "Ranger"], "Genesis": ["G70", "G80", "G90", "GV60", "GV70", "GV80"], "GMC": ["Acadia", "Canyon", "Hummer EV", "Sierra", "Terrain", "Yukon"], "Honda": ["Accord", "Civic", "CR-V", "HR-V", "Odyssey", "Passport", "Pilot", "Ridgeline"], "Hyundai": ["Elantra", "Ioniq 5", "Ioniq 6", "Kona", "Palisade", "Santa Fe", "Sonata", "Tucson", "Venue"], "Infiniti": ["Q50", "QX50", "QX55", "QX60", "QX80"], "Jaguar": ["E-Pace", "F-Pace", "F-Type", "I-Pace", "XF"], "Jeep": ["Cherokee", "Compass", "Gladiator", "Grand Cherokee", "Renegade", "Wagoneer", "Grand Wagoneer", "Wrangler"], "Kia": ["Carnival", "EV6", "Forte", "K5", "Niro", "Rio", "Seltos", "Sorento", "Soul", "Sportage", "Stinger", "Telluride"], "Lamborghini": ["Aventador", "Countach", "Huracan", "Urus"], "Land Rover": ["Defender", "Discovery", "Discovery Sport", "Range Rover", "Range Rover Evoque", "Range Rover Sport", "Range Rover Velar"], "Lexus": ["ES", "GX", "IS", "LC", "LS", "LX", "NX", "RC", "RX", "RZ", "UX"], "Lincoln": ["Aviator", "Corsair", "Nautilus", "Navigator"], "Maserati": ["Ghibli", "Grecale", "Levante", "MC20", "Quattroporte"], "Mazda": ["CX-30", "CX-5", "CX-50", "CX-9", "Mazda3", "MX-5 Miata"], "McLaren": ["720S", "Artura", "GT"], "Mercedes-Benz": ["A-Class", "C-Class", "CLA", "CLS", "E-Class", "EQB", "EQE", "EQS", "G-Class", "GLA", "GLB", "GLC", "GLE", "GLS", "S-Class", "SL-Class"], "Mitsubishi": ["Eclipse Cross", "Mirage", "Outlander", "Outlander Sport"], "Nissan": ["370Z", "Altima", "Armada", "Frontier", "GT-R", "Kicks", "Leaf", "Maxima", "Murano", "Pathfinder", "Rogue", "Sentra", "Titan", "Versa", "Z"], "Porsche": ["718 Boxster", "718 Cayman", "911", "Cayenne", "Macan", "Panamera", "Taycan"], "Ram": ["1500", "2500", "3500"], "Rolls-Royce": ["Cullinan", "Ghost", "Phantom", "Spectre"], "Subaru": ["Ascent", "BRZ", "Crosstrek", "Forester", "Impreza", "Legacy", "Outback", "Solterra", "WRX"], "Tesla": ["Model 3", "Model S", "Model X", "Model Y"], "Toyota": ["4Runner", "bZ4X", "Camry", "Corolla", "Corolla Cross", "Crown", "GR86", "Highlander", "Land Cruiser", "Prius", "RAV4", "Sequoia", "Sienna", "Supra", "Tacoma", "Tundra"], "Volkswagen": ["Arteon", "Atlas", "Golf GTI", "Golf R", "ID.4", "Jetta", "Taos", "Tiguan"], "Volvo": ["C40 Recharge", "S60", "S90", "V60", "V90", "XC40", "XC60", "XC90"]
 };
+
+const currencyList = [
+    { code: "AED", name: "United Arab Emirates Dirham" },
+    { code: "AUD", name: "Australian Dollar" },
+    { code: "BRL", name: "Brazilian Real" },
+    { code: "CAD", name: "Canadian Dollar" },
+    { code: "CHF", name: "Swiss Franc" },
+    { code: "CLP", name: "Chilean Peso" },
+    { code: "CNY", name: "Chinese Yuan" },
+    { code: "COP", name: "Colombian Peso" },
+    { code: "CZK", name: "Czech Koruna" },
+    { code: "DKK", name: "Danish Krone" },
+    { code: "EUR", name: "Euro" },
+    { code: "GBP", name: "British Pound Sterling" },
+    { code: "HKD", name: "Hong Kong Dollar" },
+    { code: "HUF", name: "Hungarian Forint" },
+    { code: "IDR", name: "Indonesian Rupiah" },
+    { code: "ILS", name: "Israeli New Shekel" },
+    { code: "INR", name: "Indian Rupee" },
+    { code: "JPY", name: "Japanese Yen" },
+    { code: "KRW", name: "South Korean Won" },
+    { code: "MXN", name: "Mexican Peso" },
+    { code: "MYR", name: "Malaysian Ringgit" },
+    { code: "NOK", name: "Norwegian Krone" },
+    { code: "NZD", name: "New Zealand Dollar" },
+    { code: "PHP", name: "Philippine Peso" },
+    { code: "PLN", name: "Polish Złoty" },
+    { code: "RON", name: "Romanian Leu" },
+    { code: "RUB", name: "Russian Ruble" },
+    { code: "SAR", name: "Saudi Riyal" },
+    { code: "SEK", name: "Swedish Krona" },
+    { code: "SGD", name: "Singapore Dollar" },
+    { code: "THB", name: "Thai Baht" },
+    { code: "TRY", name: "Turkish Lira" },
+    { code: "TWD", name: "New Taiwan Dollar" },
+    { code: "USD", name: "United States Dollar" },
+    { code: "USDT", name: "Tether" },
+    { code: "ZAR", name: "South African Rand" }
+].sort((a, b) => a.name.localeCompare(b.name));
